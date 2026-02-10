2630f57306ecef5ccd1223f45b7282d49862be8e
Philip Mdicullar <phillip.emdokolo@student.moringaschool.com>
2026-02-10 14:47:22 +0300

    fixing nav

--- Frontend/Sendit/src/components/Navbar.jsx
+++ Frontend/Sendit/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './nav.css';
-import logo from '../assets/logo.png';
+import Logo from '../assets/logo.png';
 export default function Navbar() {
   return (
     <header className="site-navbar">
       <div className="container">
-        <div className="brand"><img src={logo} alt="Sendit Logo" srcset="" /></div>
+        <div className="brand"><img src={Logo} alt="Sendit Logo" srcset="" /></div>
         <nav className="nav-links">
           <Link to="/">Home</Link>
           <Link to="/track">Track</Link>
