c6631c19a4dfe15e2953e79f8f3ae00914dbfc9d
Philip Mdicullar <phillip.emdokolo@student.moringaschool.com>
2026-02-13 00:08:08 +0300

    improving mobile responsiveness

--- Frontend/Sendit/src/sections/HowItWorksSection.jsx
+++ Frontend/Sendit/src/sections/HowItWorksSection.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './how.css';
-import Create from '../assets/how/create.png';
-import Order from '../assets/how/order.png';
-import Track from '../assets/how/Track.png';
-import Deliver from '../assets/how/Deliver.png';
+import Profile from '../assets/how/Profile.png';
+import Create from '../assets/how/Create.png'
+import Navigate from '../assets/how/Navigate.png'
+import Updates from '../assets/how/Updates.png'
+import '../components/feature.css';
 export default function HowItWorksSection() {
 
   return (
@@ -11,10 +12,26 @@ export default function HowItWorksSection() {
       <div className="container">
         <h2>How It Works</h2>
         <div className="steps">
-          <img src={Create} alt="Create" srcset="" />
-          <img src={Order} alt="Order" srcset="" />
-          <img src={Track} alt="Track" srcset="" />
-          <img src={Deliver} alt="Deliver" srcset="" />
+          <div className="step">
+            <img src={Profile} alt="Profile" />
+            <h4>Create an account</h4>
+            <p className="style">Manage it</p>
+          </div>
+          <div className="step">
+            <img src={Create} alt="Profile" />
+            <h4>Create a parcel order</h4>
+            <p className="style">Add a new order</p>
+          </div>
+          <div className="step">
+            <img src={Navigate} alt="Navigate" />
+            <h4>Track parcel in real time</h4>
+            <p className="style">See where it is</p>
+          </div>
+          <div className="step">
+            <img src={Updates} alt="Updates" />
+            <h4>Receive delivery updates</h4>
+            <p className="style">Get real time updates</p>
+          </div>
         </div>
       </div>
     </section>
